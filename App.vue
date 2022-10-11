f818943248f79d7712b894255747f9603e04b688
Oliver Wilkes <91676214+Oliver-Wilkes@users.noreply.github.com>
2022-10-11 12:22:14 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  
+<header>
+  <Navbar msg="fdfds"/>
+</header>
+
+
+  <body>
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+  </body>
+
+
+
+  <footer>
+
+  </footer>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Navbar
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
